e7a78111e9185a400ad613e1a3ebc36b6df596d0
jin camou and Kat Gifford <kat@>
2017-04-20 15:14:07 -0700

    add and pass final spec

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb') # this line allows us to see changes in sinatra w/o re starting the server
-require('./lib/title_case') #
+require('./lib/coin_combination.rb') #
 require('pry')
 
 get('/') do
